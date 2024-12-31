3b46d8fb7a390d4304127d3719a7ea046e1096f3
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-12-31 02:21:46 +0200

    Complete exercise 9.16

--- exercise-9.15-9.16/src/components/Content.tsx
+++ exercise-9.15-9.16/src/components/Content.tsx
@@ -1,19 +1,8 @@
-interface ContentProps {
-  parts: Array<{ name: string; exerciseCount: number }>;
-}
+import { CoursePart } from "../App";
+import { Part } from "./Part";
 
-export const Content = (props: ContentProps) => {
-  return (
-    <>
-      <p>
-        {props.parts[0].name} {props.parts[0].exerciseCount}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exerciseCount}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exerciseCount}
-      </p>
-    </>
-  );
+export const Content = ({ parts }: { parts: CoursePart[] }) => {
+  return parts.map((part, key) => {
+    return <Part key={key} part={part} />;
+  });
 };
---
+++ exercise-9.15-9.16/src/components/Part.tsx
@@ -0,0 +1,60 @@
+import { CoursePart } from "../App";
+
+export const Part = ({ part }: { part: CoursePart }) => {
+  let content;
+
+  const exhaustiveCheck = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member ${JSON.stringify(value)}`
+    );
+  };
+
+  switch (part.kind) {
+    case "basic":
+      content = (
+        <div>
+          <h3>{part.name}</h3>
+          <p> {part.exerciseCount} </p>
+          <p>{part.description} </p>
+          <p>{part.kind}</p>
+        </div>
+      );
+      break;
+    case "group":
+      content = (
+        <div>
+          <h3>{part.name}</h3>
+          <p> {part.exerciseCount} </p>
+          <p>{part.groupProjectCount} </p>
+          <p>{part.kind}</p>
+        </div>
+      );
+      break;
+    case "background":
+      content = (
+        <div>
+          <h3>{part.name}</h3>
+          <p> {part.exerciseCount} </p>
+          <p>{part.description} </p>
+          <p>{part.backgroundMaterial} </p>
+          <p>{part.kind}</p>
+        </div>
+      );
+      break;
+    case "special":
+      content = (
+        <div>
+          <h3>{part.name}</h3>
+          <p> {part.exerciseCount} </p>
+          <p>{part.description} </p>
+          <p>{part.requirements}</p>
+          <p>{part.kind}</p>
+        </div>
+      );
+      break;
+    default:
+      exhaustiveCheck(part);
+  }
+
+  return content;
+};
