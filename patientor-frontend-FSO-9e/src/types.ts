c93b21af0e74fba455d38f204c93fc75fd30ed7b
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2025-01-03 21:07:41 +0200

    Update directory for patientor frontend

---
+++ patientor-frontend-FSO-9e/src/types.ts
@@ -0,0 +1,29 @@
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+  occupation: string;
+  gender: Gender;
+  ssn?: string;
+  dateOfBirth?: string;
+}
+
+export interface Entry {
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCode: string;
+}
+
+export type PatientFormValues = Omit<Patient, "id" | "entries">;
